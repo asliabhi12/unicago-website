80085105bba2677a854ab768e7b3c6c276e66e93
asliabhi12 <asliabhi12@gmail.com>
2024-04-07 19:35:33 +0530

    some nominal changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,21 +6,21 @@ const Footer = () => {
   return (
     
 
-<footer className="bg-white dark:bg-gray-900 mt-20">
+<footer className="bg-white dark:bg-gray-900 mt-20 md:mb-0 mb-16">
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a href="/" className="flex items-center ">
-                  <Image src="/uni-logo-white.svg"  className="h-8 me-4 fill-white" alt="Unicago Logo" />
+                  <Image src="/uni-logo-white.svg" width={24} height={24} className="h-8 me-4 fill-white" alt="Unicago Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Unicago</span>
               </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Services</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                          <a href="https://unicago.com/" className="hover:underline">Unicago</a>
                       </li>
                       <li>
                           <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
@@ -31,7 +31,7 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                          <a href="https://github.com/asliabhi12" className="hover:underline ">Github</a>
                       </li>
                       <li>
                           <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
