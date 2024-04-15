21ea2df7c5003d560b1a9c5eaa9ad6066266337f
asliabhi12 <asliabhi12@gmail.com>
2024-04-15 22:25:23 +0530

    aading contact form

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContactUsForm from '@/components/ContactUsForm';
+
+
+export default function ContactUs() {
+  return (
+    <main className="min-h-screen bg-black/[0/0.96] antialiased bg-grid-white/[0.02]" >
+     
+        <ContactUsForm/>
+    </main>
+  );
+}
+
---
+++ src/components/ContactUsForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Input } from '@/components/ui/input'
+
+const ContactUsForm = () => {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-[100vh] w-[70vw]'>
+        <div className="">hello</div>
+        <Input type="email" placeholder='email'></Input>
+    </div>
+  )
+}
+
+export default ContactUsForm
